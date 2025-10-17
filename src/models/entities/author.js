d41506a3912c22cc2546c408fb65e1f59652bc42
Ítalo <163360684+iti0027@users.noreply.github.com>
2025-10-17 01:54:52 +0000

    criando a entidade author

--- src/models/entities/author.js
+++ src/models/entities/author.js
@@ -1,3 +1,33 @@
-const author = (sequelize, {DataTypes}) => {
-    const Author = sequelize.define("author",{})
+const getAuthorModel = (sequelize, {DataTypes}) => {
+    const author = sequelize.define("Author",{
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            unique: true,
+            allowNull: false,
+            primaryKey: true,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        name: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        nationality: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        birthDate: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+    });
+
+    author.associate = (models) => {
+        author.hasMany (models.book, {onDelete:"CASCADE"});
+    };
+    return author;
 };
+
+export default getAuthorModel;

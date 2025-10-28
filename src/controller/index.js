baaa57a27bed9489bc259e4a384377a5d134c16c
Thiago Henrique <79538085+thiagohfmelo@users.noreply.github.com>
2025-10-28 22:54:51 +0000

    adicionando o crud da entidade book no index.

---
+++ src/controller/index.js
@@ -0,0 +1,5 @@
+import { getAllBooks, getBookById, createBook, updateBookById, deleteBookById} from "./book";
+
+const book = {getAllBooks, getBookById, createBook, updateBookById, deleteBookById};
+
+export { book };

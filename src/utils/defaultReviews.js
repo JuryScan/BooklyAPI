bb1b3d27de3de462c344c4c291bd866a957b2aa4
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-11-05 20:36:52 -0300

    feat: Adicionar criação de default reviews

---
+++ src/utils/defaultReviews.js
@@ -0,0 +1,16 @@
+const reviews = [
+    {
+        rate: 3,
+        comment: "Gostei mais ou menos desse livro. Mediano...",
+    },
+    {
+        rate: 5,
+        comment: "Excelente livro! Recomendo a todos os amantes de ficção científica.",
+    },
+    {
+        rate: 4,
+        comment: "Muito bom, mas achei o final um pouco previsível.",
+    }
+]
+
+export default reviews;
